5bf9927471867af16e0267b354f89dcd8d69bbde
Momen Khaled <momenk208@gmail.com>
2023-08-14 20:04:43 +0300

    Time: 47 ms (91.90%) | Memory: 41.9 MB (57.66%) - LeetSync

---
+++ 2746-filter-elements-from-array/filter-elements-from-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    let newArr =[]
+
+    for(let i=0;i<arr.length;i++){
+        if(fn(arr[i],i)) newArr.push(arr[i])
+    }
+    return newArr
+};

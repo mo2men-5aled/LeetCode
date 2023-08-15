2ec140fa497a8fe069358e0b382685aeb74bba03
Momen Khaled <momenk208@gmail.com>
2023-08-15 18:38:58 +0300

    Time: 84 ms (92.69%) | Memory: 48.5 MB (29.99%) - LeetSync

---
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function (nums1, nums2) {
+  let arr = nums1.concat(nums2).sort((a, b) => a - b);
+  let len = arr.length;
+  if (len % 2 === 0) {
+    return (arr[len / 2 - 1] + arr[len / 2]) / 2;
+  } else {
+    return arr[Math.floor(len / 2)];
+  }
+};
